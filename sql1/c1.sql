3976fa1e8045c6f90e2493767927af12e55b4482
kaka-code <wxmaru@163.com>
2020-09-25 13:45:58 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交  说明将会终止提交。
     位于分支 master
    
     初始提交
    
     要提交的变更：
    	新文件：   sql1/c1.sql
    	新文件：   sql1/conn1.bak.zip
    	新文件：   sql1/insert1.sql
    	新文件：   sql1/q1.sql

---
+++ sql1/c1.sql
@@ -0,0 +1,14 @@
+use test;
+
+DROP TABLE IF EXISTS city;
+
+CREATE TABLE city (
+id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '城市编号',
+province_id int(10) unsigned NOT NULL COMMENT '省份编号',
+city_name varchar(25) DEFAULT NULL COMMENT '城市名称',
+description varchar(25) DEFAULT NULL COMMENT '描述',
+PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+commit;
---
+++ sql1/insert1.sql
@@ -0,0 +1,2 @@
+use test;
+insert into city values ('1','1101','北京','首都');
